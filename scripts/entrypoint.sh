9b0632f1f14efbd71536faba1fc5555b5b3fbfbc
Alex C <alexnderchung@gmail.com>
2020-05-30 04:55:01 -0400

    Added entrypoint file

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#Mounts all devices described at /etc/fstab.
+sudo mount -a
+
+AIRFLOW_HOME="/usr/local/airflow"
+CMD="airflow"
+
+#Run 'airflow initdb'
+$CMD initdb
+
+#Replace current shell and run 'airflow webserver'
+exec $CMD "$@"
