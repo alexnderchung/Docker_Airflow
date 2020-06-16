5cb7a533d3b62b5816305472fa6d8a82f32138cd
Alex C <alexnderchung@gmail.com>
2020-06-16 02:43:50 -0400

    Adding text to see if commands get run

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,10 +8,14 @@ CMD="airflow"
 
 #Run 'airflow initdb'
 sleep 30
+echo "Running airflow initdb"
 $CMD initdb
 sleep 30
+echo "Running airflow scheduler"
 $CMD scheduler
 sleep 30
+echo "Running airflow webserver"
 $CMD webserver
+echo "Finished running commands"
 #Replace current shell and run 'airflow webserver'
 #exec $CMD "$@"
