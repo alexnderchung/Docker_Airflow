235a43efb6a50dfe4038a06fa94c9ba429c9b615
Alex C <alexnderchung@gmail.com>
2020-06-30 03:20:14 -0400

    Adding Scraping Dag

---
+++ dags/Test_Dag.py
@@ -0,0 +1,56 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import airflow
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import DAG
+
+import requests
+
+import time
+import zipfile
+import os
+import pandas as pd
+import pandas_datareader.data as web
+import datetime
+
+
+def pull_BAC_data():
+    print("Start Retreiving Data...")
+    symbol = 'BAC'
+    left = web.DataReader('AAN','yahoo').shift(periods = 1).reset_index()
+    right = web.DataReader('AAN','yahoo').reset_index()
+    data = pd.merge(left = left, right = right, how = 'outer', on = 'Date', suffixes = ('Prior','Current')).dropna()
+    transformed_data = pd.concat([data, data.loc[:,data.columns.difference(['Date','VolumePrior','VolumeCurrent'])].div(data.OpenCurrent, axis = 0).round(decimals = 3).add_suffix('Percent')], axis = 1)
+    transformed_data.to_csv(f'~/Documents/{symbol}.csv')
+    print("Data Retrieved!")
+
+
+def pull_AAN_data():
+    print("Start Retreiving Data...")
+    symbol = 'AAN'
+    left = web.DataReader('AAN','yahoo').shift(periods = 1).reset_index()
+    right = web.DataReader('AAN','yahoo').reset_index()
+    data = pd.merge(left = left, right = right, how = 'outer', on = 'Date', suffixes = ('Prior','Current')).dropna()
+    transformed_data = pd.concat([data, data.loc[:,data.columns.difference(['Date','VolumePrior','VolumeCurrent'])].div(data.OpenCurrent, axis = 0).round(decimals = 3).add_suffix('Percent')], axis = 1)
+    transformed_data.to_csv(f'~/Documents/{symbol}.csv')
+    print("Data Retrieved!")
+    
+    
+# dag
+args = {"owner": "Scrape test", "start_date": airflow.utils.dates.days_ago(2)}
+
+dag = DAG(dag_id="first_exercise", default_args=args, schedule_interval=None)
+
+
+# tasks
+BAC_Task = PythonOperator(task_id="pull_BAC_data", python_callable=pull_BAC_data, dag=dag)
+
+AAN_Task = PythonOperator(task_id="pull_AAN_data", python_callable=pull_AAN_data, dag=dag)
+
+
+# dependencies
+BAC_Task.set_upstream(AAN_Task)
+
